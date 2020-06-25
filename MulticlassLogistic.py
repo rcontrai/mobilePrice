371ff20a625d576feb0c54535bf93e472aa1959b
Guillermo Martín Sánchez <guillemartinsan@gmail.com>
2020-06-25 19:37:22 +0200

    Changed Logistic into a class, used kfolds and learningcurve and our datasets

--- MulticlassLogistic.py
+++ MulticlassLogistic.py
@@ -7,7 +7,7 @@ Coursework 3: Multi-class logistical regression and neural networks
 Imports and definitions
 """
 
-from data import carga_csv, polynomial,accuracy,f1score_multi
+from data import carga_csv, polynomial,accuracy,f1score_multi, kfolds, learningcurve
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
@@ -19,19 +19,20 @@ def sigmoid(z):
     """
     return 1 / (1 + np.exp(-z))
 
-def coste_reg(theta, X, Y, lamb):
-    """
-    cost function with regularization
-    computes J(theta) for a given dataset 
-    with a regularization factor of lamb
-    """
-    m = np.shape(X)[0]
-    H = sigmoid((np.dot(X,theta)))
-    J = -1/m * ( np.log(H).transpose().dot(Y)
-                 + np.log(1-H).transpose().dot(1-Y)) 
-    reg = lamb/(2*m)*np.sum(theta[1:]**2)
-    return J + reg
 
+def coste_reg(theta, X, Y, lamb):
+        """
+        cost function with regularization
+        computes J(theta) for a given dataset 
+        with a regularization factor of lamb
+        """
+        m = np.shape(X)[0]
+        H = sigmoid((np.dot(X,theta)))
+        J = -1/m * ( np.log(H).transpose().dot(Y)
+                     + np.log(1-H).transpose().dot(1-Y)) 
+        reg = lamb/(2*m)*np.sum(theta[1:]**2)
+        return J + reg
+    
 def gradiente_reg(theta, X, Y, lamb):
     """
     gradient function with regularization
@@ -47,125 +48,112 @@ def gradiente_reg(theta, X, Y, lamb):
     reg = (lamb/m)*reg*theta
     return G + reg
 
-def predict(theta,Xtest):
-    """
-    computes the prediction for the model theta and examples Xtest
-    """
-    H = sigmoid(np.dot(Xtest,theta.T))
-    return np.argmax(H,axis = 1)
-
-
-def oneVsAll(X,y,num_etiquetas,reg):
-    """
-    one-vs-all logistical regression
-    trains num_etiquetas logistical classifiers to classify as many classes
-    uses dataset X and true target variable y
-    uses regularization with factor reg
-    returns a matrix containing each models' parameters in its lines
-    """
-    m = np.shape(X)[0]
-    n = np.shape(X)[1]
+class MulticlassLogistic:
     
-    Th = np.zeros((num_etiquetas, n))
-    # For each class
-    for i in range(num_etiquetas):
-        # Mark the points as belonging to the class or not
-        labels = np.zeros((m,1))
-        labels[y == i] = 1  # the class at index 0 has value 1
-        labels = labels.ravel()
+    Th = [];
+
+    
+    def predict(self,Xtest):
+        """
+        computes the prediction for the model theta and examples Xtest
+        """
+        mtest = np.shape(Xtest)[0]
+        Xtest = np.hstack([np.ones([mtest,1]),Xtest])
         
-        theta = np.zeros(n,)
-        # Compute the optimal value for theta
-        result = opt.fmin_tnc(func=coste_reg, x0=theta, fprime=gradiente_reg,
-                                        args=(X,labels,reg), messages = 0)
-        Th[i,:] = result[0] 
+        H = sigmoid(np.dot(Xtest,self.Th.T))
+        return np.argmax(H,axis = 1)
+    
+    
+    def fit(self,X,y,num_etiquetas,reg):
+        """
+        one-vs-all logistical regression
+        trains num_etiquetas logistical classifiers to classify as many classes
+        uses dataset X and true target variable y
+        uses regularization with factor reg
+        returns a matrix containing each models' parameters in its lines
+        """
+        m = np.shape(X)[0]
+        n = np.shape(X)[1]
         
-    return Th
+        X = np.hstack([np.ones([m,1]),X])
+        
+        Th = np.zeros((num_etiquetas, n+1))
+        # For each class
+        for i in range(num_etiquetas):
+            # Mark the points as belonging to the class or not
+            labels = np.zeros((m,1))
+            labels[y == i] = 1  # the class at index 0 has value 1
+            labels = labels.ravel()
+            
+            theta = np.zeros(n+1,)
+            # Compute the optimal value for theta
+            result = opt.fmin_tnc(func=coste_reg, x0=theta, fprime=gradiente_reg,
+                                            args=(X,labels,reg), messages = 0)
+            Th[i,:] = result[0] 
+            
+        self.Th = Th
 
 
 
 #%% 
 """
-1 - Multi-class logistical regression 
+1 - Load data
 """
 
+K = 4  # number of classes
 # loading the data
-data = carga_csv("data/train.csv")
+datatrain = carga_csv("data/train.csv")
+datatest = carga_csv("data/test.csv")
 
-np.random.seed(0) # To get consistent results while debugging
-np.random.shuffle(data)
-
-# Dividing the set into training, validation and test sets
-m = np.shape(data)[0]
-train = int(np.floor(0.6*m))
-val = int(train + np.floor(0.3*m))
-
-Xtrain = data[0:train,:-1]
-ytrain = data[0:train,-1]
-Xval = data[train:val,:-1]
-yval = data[train:val,-1]
-Xtest = data[val::,:-1]
-ytest = data[val::,-1]
+Xtrain = datatrain[:,:-1]
+ytrain = datatrain[:,-1]
+Xtest = datatest[:,:-1]
+ytest = datatest[:,-1]
 
 mtrain = np.shape(Xtrain)[0]
-mval = np.shape(Xval)[0]
 mtest = np.shape(Xtest)[0]
 
-K = 4  # number of classes
+# Split into training and validation set
+np.random.seed(0) # To get consistent results while debugging
+np.random.shuffle(datatrain)
+split = int(np.floor(0.7*mtrain))
+
+Xt_notval = datatrain[0:split,:-1]
+yt_notval = datatrain[0:split,-1]
+Xt_val = datatrain[split::,:-1]
+yt_val = datatrain[split::,-1]
+
+
+# Object used
+ML = MulticlassLogistic();
+
+# Lambdas needed
+predict = lambda x: ML.predict(x) 
+error = lambda p,y: 1 - f1score_multi(p,y,K)
+
+#%%
+"""
+2 - Multiclass Logistic 
+"""
+
 reg = 0 # regularization factor
 
-# Training the models
-Xtrainp = np.hstack([np.ones([mtrain,1]),Xtrain])
-Th = oneVsAll(Xtrainp,ytrain,K,reg)
+
+# Plot learning curve.
+batch = 10;
+fit = lambda x,y: ML.fit(x,y,K,reg)
+learningcurve(Xt_notval,yt_notval,Xt_val,yt_val,fit,predict,error,batch)
 
 # Computing precision
-
-Xtestp = np.hstack([np.ones([mtest,1]),Xtest])
-pred = predict(Th,Xtestp)
+ML.fit(Xtrain,ytrain,K,reg)
+pred = ML.predict(Xtest)
 f1 = f1score_multi(pred,ytest,K)
 e = accuracy(pred,ytest)
 print('Logistical model accuracy: {0:1.3g}, F1: {1:1.3g}'.format(e,f1))
 
 #%%
 """
-2 - Plot bias-variance plot
-"""
-
-batch = 10;
-prec = np.zeros(int(mtrain/batch)-1,)
-precval = np.zeros(int(mtrain/batch)-1,)
-
-Xvalp = np.hstack([np.ones([mval,1]),Xval])
-
-for i in range(1,int(mtrain/batch)):
-    Xi = Xtrainp[0:i*batch]
-    yi = ytrain[0:i*batch]
-    
-    Th = oneVsAll(Xi,yi,K,reg)
-    
-    pred = predict(Th,Xi)
-    prec[i-1] = 1 - f1score_multi(pred,yi,K)
-    # prec[i-1] = 1 - accuracy(pred,ytest)
-        
-    pred = predict(Th,Xvalp)
-    precval[i-1] = 1 - f1score_multi(pred,yval,K)
-    # precval[i-1] = 1 - accuracy(pred,yval)
-    
-
-# Display the learning curves
-plt.figure()
-plt.plot(prec, label = "Train")
-plt.plot(precval, label = "Cross validation")
-plt.legend()
-plt.xlabel('Iterations')
-plt.ylabel('Error')
-plt.title('Learning curve for logistic regression')
-plt.show()
-
-
-#%%
-"""
-3 - High variance - Lambda
+3A - Choose lambda with single split
 """
 
 # Train the model for a range of values of lambda
@@ -182,79 +170,106 @@ precval = np.zeros(lpts,)
 i = 0;
 for lamb in lamb_arr:
 
-    Th = oneVsAll(Xtrainp,ytrain,K,lamb)
+    ML.fit(Xt_notval,yt_notval,K,lamb)
        
-    pred = predict(Th,Xi)
-    prec[i-1] = 1 - f1score_multi(pred,yi,K)
-    # prec[i-1] = 1 - accuracy(pred,ytest)
+    pred = ML.predict(Xt_notval)
+    prec[i-1] = error(pred,yt_notval)
     
-    pred = predict(Th,Xvalp)
-    precval[i-1] = 1 - f1score_multi(pred,yval,K)
-    # precval[i-1] = 1 - accuracy(pred,yval)
+    pred = ML.predict(Xt_val)
+    precval[i-1] = error(pred,yt_val)
     i = i+1;
     
-# Display the learning curves
+# Display the lambda curve
 plt.figure()
-plt.plot(lamb_arr, prec, label = "Train")
-plt.plot(lamb_arr, precval, label = "Cross validation")
+plt.plot(lamb_arr, prec, label = "Train", alpha = 0.5)
+plt.plot(lamb_arr, precval, label = "Validation")
 plt.legend()
 plt.xlabel('$\lambda$')
 plt.ylabel('Error') 
-plt.title(r'Selecting $\lambda$ using a cross-validation set')
+plt.title(r'Selecting $\lambda$ using a validation set')
 plt.show()
 
 bestlamb = np.argmin(precval)*step
 
-print('Best lambda = : {}'.format(np.argmin(precval) * step))
+print('Best lambda with single split = : {}'.format(bestlamb))
+
+#%%
+"""
+4A - Model with best lambda with single split
+"""
+
+reg = bestlamb # regularization factor
+
+# Learning curve
+fit = lambda x,y: ML.fit(x,y,K,reg)
+batch = 10
+learningcurve(Xt_notval,yt_notval,Xt_val,yt_val,fit,predict,error,batch)
+
+
+# Computing precision
+ML.fit(Xtrain,ytrain,K,reg)
+pred = ML.predict(Xtest)
+f1 = f1score_multi(pred,ytest,K)
+e = accuracy(pred,ytest)
+print('Logistical model accuracy with single split lambda = {0}: {1:1.3g}, F1: {2:1.3g}'.format(reg,e,f1))
+
+#%%
+"""
+3B - Choose lambda with kfolds
+"""
+
+
+k = 5; # number of folds
+step = 10;
+lamb_arr = range(0,1000,step)
+lpts = np.size(lamb_arr)
+
+prec = np.zeros(lpts,)
+precval = np.zeros(lpts,)
+
+i = 0;
+for lamb in lamb_arr:
+
+    fit = lambda x,y: ML.fit(x,y,K,lamb)
+    
+    prec[i],precval[i] = kfolds(Xtrain,ytrain,fit,predict,error,k)
+    i = i +1;
+    
+    
+# Display the lambda curves
+plt.figure()
+plt.plot(lamb_arr, prec, label = "Train", alpha = 0.5)
+plt.plot(lamb_arr, precval, label = "Average validation")
+plt.legend()
+plt.xlabel('$\lambda$')
+plt.ylabel('Error') 
+plt.title(r'Selecting $\lambda$ using k-folds')
+plt.show()
+
+bestlambkfolds = np.argmin(precval)*step
+
+print('Best lambda with kfolds = : {}'.format(bestlambkfolds))
 
 
 #%%
 """
-4 - Model with best lambda
+4B - Model with best lambda with kfolds
 """
-K = 4  # number of classes
-reg = bestlamb# regularization factor
 
-# Training the models
-Th = oneVsAll(Xtrainp,ytrain,K,reg)
+reg = bestlambkfolds # regularization factor
+
+# Learning curve
+fit = lambda x,y: ML.fit(x,y,K,reg)
+batch = 10
+learningcurve(Xt_notval,yt_notval,Xt_val,yt_val,fit,predict,error,batch)
+
 
 # Computing precision
-
-Xtestp = np.hstack([np.ones([mtest,1]),Xtest])
-pred = predict(Th,Xtestp)
+ML.fit(Xtrain,ytrain,K,reg)
+pred = ML.predict(Xtest)
 f1 = f1score_multi(pred,ytest,K)
 e = accuracy(pred,ytest)
-print('Logistical model accuracy: {0:1.3g}, F1: {1:1.3g}'.format(e,f1))
-
-
-batch = 10;
-prec = np.zeros(int(mtrain/batch)-1,)
-precval = np.zeros(int(mtrain/batch)-1,)
-
-for i in range(1,int(mtrain/batch)):
-    Xi = Xtrainp[0:i*batch]
-    yi = ytrain[0:i*batch]
-    
-    Th = oneVsAll(Xi,yi,K,reg)
-        
-    pred = predict(Th,Xi)
-    prec[i-1] = 1 - f1score_multi(pred,yi,K)
-    # prec[i-1] = 1 - accuracy(pred,ytest)
-        
-    pred = predict(Th,Xvalp)
-    precval[i-1] = 1 - f1score_multi(pred,yval,K)
-    # precval[i-1] = 1 - accuracy(pred,yval)
-    
-
-# Display the learning curves
-plt.figure()
-plt.plot(prec, label = "Train")
-plt.plot(precval, label = "Cross validation")
-plt.legend()
-plt.xlabel('Iterations')
-plt.ylabel('Error')
-plt.title('Learning curve for logistic regression')
-plt.show()
+print('Logistical model accuracy with kfolds lambda = {0}: {1:1.3g}, F1: {2:1.3g}'.format(reg,e,f1))
 
 #%%
 """
@@ -266,45 +281,29 @@ reg = 10000;
 
 d = 2;
 
-prec = np.zeros(int(mtrain/batch)-1,)
-precval = np.zeros(int(mtrain/batch)-1,)
-
+# Polynomial transformations
 Xtrain_pol, mu, sigma = polynomial(Xtrain,d)
-Xval_pol, _, _ = polynomial(Xval,d)
+Xt_notval_pol, mu_notval, sigma_notval = polynomial(Xt_notval,d)
+Xt_val_pol, _, _ = polynomial(Xt_val,d)
+Xtest_pol, _, _ = polynomial(Xtest,d)
 
-Xval_pol[:,1:] = (Xval_pol[:,1:] - mu) / sigma
+Xtrain_pol = Xtrain_pol[:,1:]
+Xt_notval_pol = Xt_notval_pol[:,1:]
+Xt_val_pol = Xt_val_pol[:,1:]
+Xtest_pol = Xtest_pol[:,1:]
 
-for i in range(1,int(mtrain/batch)):
-    Xi = Xtrain_pol[0:i*batch]
-    yi = ytrain[0:i*batch]
-    
-    Th = oneVsAll(Xi,yi,K,reg)
-    
-    pred = predict(Th,Xi)
-    prec[i-1] = 1 - f1score_multi(pred,yi,K)
-    # prec[i-1] = 1 - accuracy(pred,ytest)
-    if i == 3:
-        a = 1
-    pred = predict(Th,Xval_pol)
-    precval[i-1] = 1 - f1score_multi(pred,yval,K)
-    # precval[i-1] = 1 - accuracy(pred,yval)
-    
+Xt_val_pol = (Xt_val_pol - mu_notval) / sigma_notval
+Xtest_pol = (Xtest_pol - mu) / sigma
 
-# Display the learning curves
-plt.figure()
-plt.plot(prec, label = "Train")
-plt.plot(precval, label = "Cross validation")
-plt.legend()
-plt.xlabel('Iterations')
-plt.ylabel('Error')
-plt.title('Learning curve for logistic regression')
-plt.show()
+
+# Learning curve
+fit = lambda x,y: ML.fit(x,y,K,reg)
+learningcurve(Xt_notval_pol,yt_notval,Xt_val_pol,yt_val,fit,predict,error,batch)
+
 
 # Computing precision
-
-Xtest_pol, _, _ = polynomial(Xtest,d)
-Xtest_pol[:,1:] = (Xtest_pol[:,1:] - mu) / sigma
-pred = predict(Th,Xtest_pol)
+ML.fit(Xtrain_pol,ytrain,K,reg)
+pred = ML.predict(Xtest_pol)
 f1 = f1score_multi(pred,ytest,K)
 e = accuracy(pred,ytest)
 print('Logistical model accuracy: {0:1.3g}, F1: {1:1.3g}'.format(e,f1))
